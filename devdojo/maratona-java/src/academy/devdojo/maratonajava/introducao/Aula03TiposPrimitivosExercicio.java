60d9a7cc2a521dc058561e333c5d3ad6c2a1ad34
Ian Pereira <ian.kzw@gmail.com>
2023-04-30 11:06:50 -0300

    aula 14

--- devdojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ devdojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -4,9 +4,9 @@ public class Aula03TiposPrimitivosExercicio {
     public static void main(String[] args) {
         String name = "Ian Pereira";
         String address = "Bahia";
-        int reward = 1000;
+        double reward = 5431.12;
         String date = "15/03/2021";
-
+        // String relatorio = "Eu " + name + ", morando no endereço " + address + ", confirmo que recebi o salário de " + reward + ", na data " + date;
         System.out.println("Eu " + name + ", morando no endereço " + address + ", confirmo que recebi o salário de " + reward + ", na data " + date);
     }
 }
