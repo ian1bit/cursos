fbea4d551d11323fd3cf2e7c7780a97b5290e01c
Ian Pereira <ian.kzw@gmail.com>
2024-04-07 19:36:25 -0300

    modulo10: aula 6

---
+++ roger-melo-js/modulo10/aula3/app.js
@@ -0,0 +1,22 @@
+const past = new Date('May 1 2020 7:47:00')
+const present = new Date()
+
+const difference = present.getTime() - past.getTime()
+console.log(difference)
+
+const seconds = Math.round(difference / 1000)
+console.log({ seconds })
+
+const minutes = Math.round(seconds / 60)
+console.log({ minutes })
+
+const hours = Math.round(minutes / 60)
+console.log({ hours })
+
+const days = Math.round(hours / 24)
+console.log({ days })
+
+console.log(`Escrito há ${days} dias`)
+
+const timestamp = 1634093404039
+console.log(new Date(timestamp))
